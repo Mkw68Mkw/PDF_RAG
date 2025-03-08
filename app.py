5f0e6a2c44bea2a49db9cc1b5cd40918762c305d
Mkw68Mkw <mkw68mkw@outlook.de>
2025-03-08 12:39:58 +0100

    requirements added

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
